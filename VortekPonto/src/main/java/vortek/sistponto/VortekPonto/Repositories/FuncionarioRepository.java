90080706aab215c69035ac54290c75c8dc55256e
Thiago <teagos371@gmail.com>
2025-03-18 00:14:17 -0300

    feat: (VTK-11) Adicionada organização de branch por sprint

--- VortekPonto/src/main/java/vortek/sistponto/VortekPonto/Repositories/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package vortek.sistponto.VortekPonto.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vortek.sistponto.VortekPonto.Models.Funcionario;
-
-@Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}

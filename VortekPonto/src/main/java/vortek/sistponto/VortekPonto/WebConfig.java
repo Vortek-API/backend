c3d3578475dd9318e3bbfb6884b5287fe9c4b5f3
Thiago <teagos371@gmail.com>
2025-03-29 00:20:02 -0300

    feat: add config do cors

---
+++ VortekPonto/src/main/java/vortek/sistponto/VortekPonto/WebConfig.java
@@ -0,0 +1,18 @@
+package vortek.sistponto.VortekPonto;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")  // Permitir apenas a origem do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos HTTP permitidos
+                .allowedHeaders("*")  // Permitir todos os cabeçalhos
+                .allowCredentials(true);  // Permitir envio de credenciais (como cookies ou cabeçalhos de autenticação)
+    }
+}
